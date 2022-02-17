3ab1c46836a2329221d7fac9882f5d223b7f31f7
esther615kim <vilchen@naver.com>
2022-02-17 10:49:55 +0900

    fix: fix loading error on ProfilePage

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -3,38 +3,32 @@ import { useContext, useEffect, useState } from "react";
 import AuthContext from "../contexts/authContext";
 import { Avatar, ButtonGroup, Button } from "@mui/material";
 import { LinearProgress, Grid } from "@mui/material";
-import { useNavigate } from "react-router-dom";
 import { getPostsbyUser } from "../utils/api";
 import PostCard from "../components/Posts/PostCard";
-import { useLocation } from "react-router-dom";
 
 const ProfilePage = () => {
   const [articles, setArticles] = useState();
   const { loggedin, username } = useContext(AuthContext);
 
-  const location = useLocation();
-  const { from } = location.state; // how can I make it optional?
 
   useEffect(() => {
-    from
-      ? getPostsbyUser(from)
-      : getPostsbyUser(username).then((updatedArticles) =>
+
+       getPostsbyUser(username).then((updatedArticles) =>
           setArticles(updatedArticles)
         );
-  }, [username, from]);
+  }, [username]);
 
-  articles && console.log(articles);
 
   if (!articles) return <LinearProgress color="inherit" />;
 
   if (articles)
     return (
       <StyledBox>
-        {/* <Avatar sx={{width:100,height:100}}/> */}
-        <h3>{from? from : username}</h3>
+        <Avatar sx={{width:100,height:100}}/>
+        <h3>{username}</h3>
         <ButtonGroup color="secondary" variant="outlined">
           <Button>Articles</Button>
-          <Button>Comments</Button>
+          <Button disabled>Comments</Button>
         </ButtonGroup>
 
         <Grid
