f1ccdf860134e5035c54599b3844f6ce35c3774f
esther615kim <vilchen@naver.com>
2022-02-22 01:15:06 +0900

    fix: change baseURL

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export const dataApi = axios.create({
-    baseURL:"http://localhost:8080/api"
-    // baseURL:"https://ek-reddit.herokuapp.com/api"
+    // baseURL:"http://localhost:8080/api"
+    baseURL:"https://ek-reddit.herokuapp.com/api"
 })
 
 export const getTopics = ()=>{
